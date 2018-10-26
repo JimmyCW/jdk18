387586b27c134f365301795e0b31c8b768b4c14f
weixing <jimmywei19@163.com>
2018-10-26 18:45:42 +0800

    basic code

---
+++ src/main/java/com/wx/jdk18/cgliborjdk/jdkdynamicproxy/BookFacadeImpl.java
@@ -0,0 +1,14 @@
+package com.wx.jdk18.cgliborjdk.jdkdynamicproxy;
+
+/**
+ * @author weixing
+ * @date 2018/10/26
+ **/
+public class BookFacadeImpl implements BookFacade {
+
+    @Override
+    public void addBook() {
+        System.out.println("add book mehod");
+    }
+}
+
