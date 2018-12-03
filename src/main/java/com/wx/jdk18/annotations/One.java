bcdfdcfd9242ea89f5f672e345e220364772d3e5
JCW <jimmywei19@163.com>
2018-12-03 17:07:25 +0800

    annotations

---
+++ src/main/java/com/wx/jdk18/annotations/One.java
@@ -0,0 +1,16 @@
+package com.wx.jdk18.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author weixing
+ * @date 2018/11/29
+ **/
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = ElementType.TYPE)
+public @interface One {
+    boolean real();
+}
